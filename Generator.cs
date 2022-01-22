b7c1894e3b08672bbf0b9152922e9e5e297255b8
Lysenko Vladyslav <33177301+Vladuhalis@users.noreply.github.com>
2022-01-22 18:33:38 +0200

    Initial commit

---
+++ Generator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Generator
+{
+    class Generator
+    {
+        Random rnd;
+        private List<string> Numbers;
+        private List<string> Symbols;
+        private List<string> AlphaUpper;
+        private List<string> AlphaLower;
+        public string Password;
+
+        public Generator()
+        {
+            rnd = new Random(DateTime.Now.Millisecond);
+            Numbers = new List<string>() {"0123456789"};
+            AlphaUpper = new List<string>() {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+            AlphaLower = new List<string>() { "abcdefghijklmnopqrstuvwxyz"};
+            Symbols = new List<string>() {"!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ "};
+        }
+
+        public string PasswordGenerate(int size, bool num, bool sym, bool UpA, bool LowA)
+        {
+            Password = null;
+            string buf = null;
+            if (UpA == true)
+            {
+                foreach (string s in AlphaUpper)
+                {
+                    buf += s;
+                }
+            }
+            if (LowA == true)
+            {
+                foreach (string s in AlphaLower)
+                {
+                    buf += s;
+                }
+            }
+            if (num == true)
+            {
+
+                foreach (string s in Numbers)
+                {
+                    buf += s;
+                }
+            }
+            if (sym == true)
+            {
+                foreach (string s in Symbols)
+                {
+                    buf += s;
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Password += buf[rnd.Next(buf.Length)];
+            }
+            return Password;
+        }
+        public string UserPasswordGenerate(int size, string symbols)
+        {
+            Password = null;
+            for (int i = 0; i < size; i++)
+            {
+                Password += symbols[rnd.Next(symbols.Length)];
+            }
+            return Password;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Password_Generator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Generator gen;
+        public MainWindow()
+        {
+            InitializeComponent();
+            gen = new Generator();
+        }
+        private void Generate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Size.Text == "")
+            {
+                MessageBox.Show("Введите поле длины пароля!");
+            }
+            else
+            {
+                if (UserSymbols.Text == "")
+                {
+                    if (int.Parse(Size.Text) <= 32 && int.Parse(Size.Text) >= 4)
+                    {
+                        bool num = false, sym = false, UpA = false, LowA = false;
+                        if(checkBoxNum.IsChecked == true)
+                        {
+                            num = true;
+                        }
+                        if (checkBoxSym.IsChecked == true)
+                        {
+                            sym = true;
+                        }
+                        if (checkBoxUpA.IsChecked == true)
+                        {
+                            UpA = true;
+                        }
+                        if (checkBoxLowA.IsChecked == true)
+                        {
+                            LowA = true;
+                        }
+                        if(num == false && sym == false && UpA == false && LowA == false)
+                        {
+                            MessageBox.Show("Выберите параметры для пароля!");
+                        }
+                        else
+                        {
+                            PasswordBox.Text = gen.PasswordGenerate(int.Parse(Size.Text), num, sym, UpA, LowA);
+                        }
+                       
+                    }
+                    else
+                    {
+                        MessageBox.Show("Длина пароля должна быть в диапазоне от 4 до 32 символов!");
+                    }
+                }
+                else
+                {
+                    if (int.Parse(Size.Text) <= 32 && int.Parse(Size.Text) >= 4)
+                    {
+                        PasswordBox.Text = gen.UserPasswordGenerate(int.Parse(Size.Text), UserSymbols.Text);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Длина пароля должна быть в диапазоне от 4 до 32 символов!");
+                    }
+                }
+            }
+        }
+
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt|Word Documents|*.doc";
+            saveFileDialog.FileName = "MyPassword";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, PasswordBox.Text);
+            }
+           
+        }
+    }
+}
